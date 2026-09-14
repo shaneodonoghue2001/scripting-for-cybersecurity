e869bba7a36f1e5eae95a5dc4eb043d41404a5df
Shane <c00324599@setu.ie>
2026-09-14 14:48:13 +0000

    updated file 1

---
+++ lab01/hello.py
@@ -0,0 +1,7 @@
+name = input("What is your name? ")
+course = input("What is the course you are doing? ")
+favAreaOfCyb = input("What is your favouirite area of cyber? ")
+
+print("Hello, " + name + "!" )
+print("I see your course is " + course)
+print("And your favourite area is " + favAreaOfCyb)
