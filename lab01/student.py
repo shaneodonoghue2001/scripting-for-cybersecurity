42fc1d348d387f074234a64e1d5f5495fbd27ec8
Shane <c00324599@setu.ie>
2026-09-14 14:33:16 +0000

    Complete Lab 1

---
+++ lab01/student.py
@@ -0,0 +1,7 @@
+name = input("What is your name? ")
+id = input("What is your student ID? ")
+yearOfStudy = input("What year of your studies arer you in? ")
+
+print("Hello " + name)
+print("Student ID: " + id)
+print("Year: " + yearOfStudy)
