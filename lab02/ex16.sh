36877847ca91be8ce57fa161ccfaa4d5c0f09066
Shane <c00324599@setu.ie>
2026-09-18 08:24:26 +0000

    lab02 updated

---
+++ lab02/ex16.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+
+username="Alex" 
+hostname=$(whoami)
+current_directory=$(pwd)
+number_of_items_in_the_current_directory=$(pwd | wc -l)
+
+echo "User: $username"
+
+echo "Computer: $hostname"
+
+echo "Directory: $current_directory"
+
+echo "Items: $number_of_items_in_the_current_directory"
